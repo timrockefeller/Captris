7d05e9976062e4f3d4860b55bd9f5ecba2b752d9
kitekii <tim720719@163.com>
2020-07-23 09:15:53 +0800

    add an animation

---
+++ Assets/Animations/ScriptAnimate/Downfall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Downfall : MonoBehaviour
+{
+
+    private Vector3 SolidPosition;
+    private bool isFinished = false;
+    private void Start()
+    {
+        this.SolidPosition = this.transform.position;
+        this.transform.position += new Vector3(0, 1, 0);
+    }
+    private void Update()
+    {
+        if (!isFinished)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, this.SolidPosition, 0.03f);
+            if (Mathf.Abs(this.transform.position.y - this.SolidPosition.y) < 0.01)
+            {
+                this.transform.position = SolidPosition;
+                isFinished = true;
+            }
+        }
+    }
+}
