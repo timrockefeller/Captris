6f2611ebf92fee8bf4b7b93991f62b9ac56f826a
kitekii <tim720719@163.com>
2020-07-30 14:28:22 +0800

    wtf a ball?

---
+++ Assets/Scripts/InGame/Actions/InverseGravity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class InverseGravity : MonoBehaviour
+{
+    private void Awake() {
+            Physics.gravity = new Vector3(0, -0.1F, 0);
+    }
+}
