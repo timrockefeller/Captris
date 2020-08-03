62893869fecc050c26dc73583de9142e8d0e5297
kitekii <tim720719@163.com>
2020-08-03 17:44:10 +0800

    defend prefab

---
+++ Assets/Scripts/InGame/Defender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DefenderType
+{
+    Default
+}
+
+
+
+
+[RequireComponent(typeof(Collision))]
+public class Defender : MonoBehaviour
+{
+    public DefenderType type = DefenderType.Default;
+
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.collider.tag == "Enemy")
+        {
+            // TODO Do Attack
+
+        }
+    }
+}
