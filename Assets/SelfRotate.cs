eff06930526de74ac2376aefe9e63d9d63255605
kitekii <tim720719@163.com>
2020-08-07 16:41:57 +0800

    tower preload

---
+++ Assets/SelfRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotate : MonoBehaviour
+{
+
+    public float rotateSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.rotation
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+    }
+}
