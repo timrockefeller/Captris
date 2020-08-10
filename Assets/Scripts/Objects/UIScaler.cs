3e92efb934d970146456126143fb79e5d40471c5
kitekii <tim720719@163.com>
2020-08-10 19:22:31 +0800

    UI auto Scale

---
+++ Assets/Scripts/Objects/UIScaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private CanvasScaler canvasScaler;
+    void Start()
+    {
+        canvasScaler = GetComponent<CanvasScaler>();
+    }
+    private void Update()
+    {
+
+        canvasScaler.scaleFactor = Mathf.Lerp(canvasScaler.scaleFactor, Screen.height * 0.15f / 100f, 2 * Time.deltaTime);
+    }
+}
