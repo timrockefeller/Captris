4dd80beeca6b6f59658bf71fa107daf60b8bd382
kitekii <tim720719@163.com>
2020-08-11 11:33:43 +0800

    extend explosion function

--- Assets/Scripts/InGame/Actions/Explosion.cs
+++ Assets/Scripts/InGame/Actions/Explosion.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    public float staying = 0;
+    private float curStaying = 0;
     public float attenuation;
     void FixedUpdate()
     {
-        this.transform.localScale = Vector3.Lerp(this.transform.localScale, Vector3.zero, attenuation * Time.fixedDeltaTime);
-        if (this.transform.localScale.magnitude < 0.02f) Destroy(gameObject);
+        if (curStaying >= staying || staying == 0)
+        {
+            this.transform.localScale = Vector3.Lerp(this.transform.localScale, Vector3.zero, attenuation * Time.fixedDeltaTime);
+            if (this.transform.localScale.magnitude < 0.02f) Destroy(gameObject);
+        }
+        else
+        {
+            curStaying += Time.fixedDeltaTime;
+        }
     }
     private bool damageMutex = true;
+    public bool attackTerrain = false;
+    public bool attackPlayer = false;
+    // public bool attackEnemy = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (attackPlayer && other.tag == "Player")
         {
             if (damageMutex)
             {
@@ -21,7 +33,7 @@ public class Explosion : MonoBehaviour
                 damageMutex = false;
             }
         }
-        if (other.tag == "Terrain")
+        if (attackTerrain && other.tag == "Terrain")
         {
             other.GetComponent<TerrainUnit>().SetEmpty();
         }
